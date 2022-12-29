2500b66d67c1147e05860003576a42937d8991f1
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-12-28 21:46:53 -0300

    feat: create 'plot_avg_diff_by_budget'

--- reporter/main.py
+++ reporter/main.py
@@ -3,9 +3,9 @@ import csv
 from model.player import Player
 from model.player_collection import PlayerCollection
 
-import sys
+from textwrap import dedent
 
-from plot_gen import plot_graph
+from plot_gen import GraphGen
 
 
 # Open the CSV file
@@ -24,24 +24,41 @@ with open('data/resultados.csv', 'r') as file:
 
     player_collection.calculate_accuracy()
 
-    filename, position, budget = sys.argv
+    # filename, position, budget = sys.argv
 
-    data = []
-    for rodada in range(2, 38):
-        players = PlayerCollection.get_by_round(player_collection.players, rodada)
-        players = PlayerCollection.get_by_budget(players, float(budget))
-        players = PlayerCollection.get_by_position(players, position)
+    # data = []
+    # for rodada in range(2, 38):
+    #     players = PlayerCollection.get_by_round(player_collection.players, rodada)
+    #     players = PlayerCollection.get_by_budget(players, float(budget))
+    #     players = PlayerCollection.get_by_position(players, position)
 
-        players = sorted(players, key=lambda player: player.rank_diferenca)
+    #     players = sorted(players, key=lambda player: player.rank_diferenca)
 
-        avg_rank_difference = 0
-        for i in range(10):
-            if i == len(players): break
-            player = players[i]
-            avg_rank_difference += player.rank_diferenca
-            # print(f"{player.posicao} - {player.nome}: {player.rank} ({player.rank_diferenca}) -> {player.assertividade}")
-        avg_rank_difference /= len(players)
-        avg_rank_difference = int(avg_rank_difference)
-        data.append(avg_rank_difference)
+    #     avg_rank_difference = 0
+    #     for i in range(10):
+    #         if i == len(players): break
+    #         player = players[i]
+    #         avg_rank_difference += player.rank_diferenca
+    #         # print(f"{player.posicao} - {player.nome}: {player.rank} ({player.rank_diferenca}) -> {player.assertividade}")
+    #     avg_rank_difference /= len(players)
+    #     avg_rank_difference = int(avg_rank_difference)
+    #     data.append(avg_rank_difference)
 
-    plot_graph(range(2, 38), data)
+    answer = None
+
+    while answer != "-1":
+        answer = int(input(dedent("""
+            1. Grafico de linha com o rank do jogador recomendado para uma determinada posicao durante as 38 rodadas
+            2. Grafico de coluna com a media do ranking do jogador recomendado em uma determinada posicao para todas as rodadas e orcamentos
+            3. Grafico de coluna com a diferenca media do jogador recomendado em uma determinada posicao para todas as rodadas e orcamentos
+            4. Grafico de coluna com a diferenca media por custo para todas as posicoes e rodadas
+            >> """)))
+
+        if answer < 1 or answer > 4: 
+            print("Entrada invalida")
+            pass
+
+        graph_gen = GraphGen()
+
+        if answer == 4:
+            graph_gen.plot_avg_diff_by_budget()
--- reporter/plot_gen.py
+++ reporter/plot_gen.py
@@ -1,42 +1,111 @@
 import pandas as pd
 import plotly.graph_objects as go
-import plotly.express as px
 from plotly.subplots import make_subplots
 
 
-def plot_graph(labels=[], data=[]):
-	data = pd.read_csv("data/resultados.csv")
-	df = data[data["query_id_rodada"] == 30]
-	df = df[df["query_posicao"] == "ata"]
+ROUNDS = range(2, 38)
+POSITIONS = ['gol', 'zag', 'lat', 'mei', 'ata', 'tec']
+BUDGETS = [4.0, 8.0, 12.0]
 
-	fig = make_subplots(
-		rows=3,
-		cols=1,
-		subplot_titles=("Busca com 4 Cartoletas", "Busca com 8 Cartoletas", "Busca com 12 Cartoletas"),
-	)
 
-	for idx, budget in enumerate([4.0, 8.0, 12.0]):
-		within_budget_df = df[df["query_orcamento"] == budget].head(5)
+class GraphGen:
+	def __init__(self) -> None:
+		self.df = pd.read_csv("data/resultados.csv")
 
-		fig.append_trace(go.Bar(
-			x=within_budget_df["nome"],
-			y=within_budget_df["media_normalizada"],
-			text=within_budget_df["media_normalizada"],
-			name='Pontuação Esperada',
-			marker_color='indianred',
-		), row=idx + 1, col=1)
+	def plot_avg_diff_by_budget(self):
+		fig = make_subplots(
+			rows=3,
+			cols=2,
+			subplot_titles=(
+				list(map(lambda x: x.upper(), POSITIONS))
+			)
+		)
 
-		fig.append_trace(go.Bar(
-			x=within_budget_df["nome"],
-			y=within_budget_df["pontuacao_obtida"],
-			text=within_budget_df["pontuacao_obtida"],
-			name='Pontuação Obtida',
-			marker_color='lightsalmon',
-		), row=idx + 1, col=1)
+		plot_coords = {
+			0: (1, 1),
+			1: (1, 2),
+			2: (2, 1),
+			3: (2, 2),
+			4: (3, 1),
+			5: (3, 2)
+		}
 
-	fig.update_layout(
-		width=800,
-		title_text="Busca por atacante no mercado da rodada 30"
-	)
+		for index, position in enumerate(POSITIONS):
+			df = self.df[self.df["query_posicao"] == position]
 
-	fig.show()
+			low_avg = df[df["query_orcamento"] == 4.0]["diferenca"].abs().mean()
+			med_avg = df[df["query_orcamento"] == 8.0]["diferenca"].abs().mean()
+			hig_avg = df[df["query_orcamento"] == 12.0]["diferenca"].abs().mean()
+			all_avg = map(lambda x: round(x, 2), [low_avg, med_avg, hig_avg])
+
+			data = pd.DataFrame({
+				"budget": BUDGETS, 
+				"avg_value": all_avg
+			})
+
+			row, col = plot_coords[index]
+
+			fig.append_trace(go.Bar(
+				x=data["budget"].map(lambda x: str(x)),
+				y=data["avg_value"],
+				text=data["avg_value"],
+				marker_color="indianred",
+				showlegend=False
+			), row=row, col=col)
+
+		fig.update_layout(
+			width=800,
+			title_text="Média da diferença por posicao de todas as rodadas"
+		)
+
+		fig.show()
+
+		# positions_df = pd.DataFrame(POSITIONS)
+
+		# for index, budget in enumerate(BUDGETS):
+		# 	df = self.df
+		# 	players_within_budget_df = df[df["query_orcamento"] == budget]
+
+		
+
+# def plot_graph(answer):
+# 	data = pd.read_csv("data/resultados.csv")
+# 	df = data[data["query_id_rodada"] == 30]
+# 	df = df[df["query_posicao"] == "ata"]
+
+# 	fig = make_subplots(
+# 		rows=3,
+# 		cols=1,
+# 		subplot_titles=("Busca com 4 Cartoletas", "Busca com 8 Cartoletas", "Busca com 12 Cartoletas"),
+# 	)
+
+# 	for idx, budget in enumerate([4.0, 8.0, 12.0]):
+# 		within_budget_df = df[df["query_orcamento"] == budget].head(10)
+
+# 		fig.append_trace(go.Bar(
+# 			x=within_budget_df["nome"],
+# 			y=within_budget_df["media_normalizada"],
+# 			text=within_budget_df["media_normalizada"],
+# 			name='Pontuação Esperada',
+# 			marker_color='indianred',
+# 		), row=idx + 1, col=1)
+
+# 		fig.append_trace(go.Bar(
+# 			x=within_budget_df["nome"],
+# 			y=within_budget_df["pontuacao_obtida"],
+# 			text=within_budget_df["pontuacao_obtida"],
+# 			name='Pontuação Obtida',
+# 			marker_color='lightsalmon',
+# 		), row=idx + 1, col=1)
+
+# 	fig.update_layout(
+# 		width=800,
+# 		title_text="Busca por atacante no mercado da rodada 30"
+# 	)
+
+# 	fig.show()
+
+
+# "Diferença média com orçamento de 4 Cartoletas",
+# "Diferença média com orçamento de 8 Cartoletas",
+# "Diferença média com orçamento de 12 Cartoletas"
